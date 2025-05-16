2dfc94567416bbcaa4bc12b446129e75ae327e47
madhugilla <madhugilla@yahoo.com>
2025-05-17 01:08:44 +0530

    Add UserId and Type properties to Order model for enhanced data representation

--- cosmosofflinewithLCC/Models/Order.cs
+++ cosmosofflinewithLCC/Models/Order.cs
@@ -5,5 +5,9 @@ namespace cosmosofflinewithLCC.Models
         public string Id { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public DateTime LastModified { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+        public string Type { get; set; } = "Order";
+
     }
 }
